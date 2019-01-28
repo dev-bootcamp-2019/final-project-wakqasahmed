8c557798dad04fca8c6803061dd31035421f622c
Waqas Ahmed <wakqasahmed@gmail.com>
2019-01-28 20:00:23 +0400

    Commented rinkeby deployment configuration

--- truffle.js
+++ truffle.js
@@ -1,10 +1,12 @@
 /* For macOS and Linux Users */
 
+/* uncomment to deploy on rinkeby testnet
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const infuraKey = "fj4jll3k....."; //replace it with your infura key
 
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
+*/
 
 module.exports = {
   migrations_directory: "./migrations",
@@ -26,12 +28,14 @@ module.exports = {
       port: 8545,
       network_id: 3,
       from: "0xe7d6c3f43d7859d7d6d045f9ac460eedffd3eae6" // replace with your Testnet account address
-    }*/
+    },
+    */
+    /* uncomment to deploy on rinkeby testnet
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
       network_id: 4,          // Rinkeby's id
       gas: 5500000,        
     },
-
+    */
   }
 };
